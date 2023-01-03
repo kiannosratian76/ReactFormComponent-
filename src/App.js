32f87b30e12f8cf1ae42075da9477ba93aa5bd03
kiannosratian <kiannosratian76@gmail.com>
2023-01-04 01:08:54 +0330

    this is First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+class Form extends React.Component {
+    // function for get input value and set to the state
+    sumitButton = (e) => {
+        e.preventDefault();
+        console.log("form Submited");
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() {
+        //  this is jsx html Code for return container for  html in React
+        return (
+            <div className="Container-root">
+                <div className="formContainer">
+                    {/*Header */}
+                    <header>
+                        <h1>React_Component</h1>
+                    </header>
+                    {/* Container wrapper */}
+                    <div className="Wrapper">
+                        <ul className="Wrapper-menu">
+                            {/* li items menu  */}
+                            <li className="wrapper-items">
+                                <p className="Wr-items-para">FirstItems</p>
+                                <input />
+                                <p className="wrapper-btn">
+                                    <i>TIk</i>_<i>Close</i>
+                                </p>
+                            </li>
+                            <li className="wrapper-items">
+                                <p className="Wr-items-para">SecondItems</p>
+                                <input />
+                                <p className="wrapper-btn">
+                                    <i>TIk</i>_<i>Close</i>
+                                </p>
+                            </li>
+                            <li className="wrapper-items">
+                                <p className="Wr-items-para">ThreeItems</p>
+                                <input />
+                                <p className="wrapper-btn">
+                                    <i>TIk</i>_<i>Close</i>
+                                </p>
+                            </li>
+                        </ul>
+                        {/* form for plus element to state  */}
+                        <div className="form-wrapper">
+                            <form onSubmit={this.sumitButton}>
+                                {/* Get Element name  */}
+                                <input
+                                    id="input-from"
+                                    placeholder="insert the name"
+                                />
+                                {/* Submit Form and import the element with submit form */}
+                                <button id="clickBtn">ClickMe</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Form;
